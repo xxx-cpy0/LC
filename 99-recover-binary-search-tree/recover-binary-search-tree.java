51f78667faccaadcdd41185ae8301566e04ba37c
xxx-cpy0 <mayil12102005@gmail.com>
2026-06-20 20:34:49 +0530

    Time: 2 ms (16.81%) | Memory: 46.5 MB (68.27%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -25,10 +25,8 @@ class Solution {
         List<TreeNode> l = new ArrayList<>();
         TreeNode cpy = root;
         inorder(cpy,l);
-        TreeNode temp = new TreeNode(0);
         TreeNode first = null;
         TreeNode second = null;
-        for(int i=0;i<l.size();i++) System.out.print(l.get(i).val+" ");
         for(int i=1;i<=l.size()-1;i++){
             if(l.get(i-1).val < l.get(i).val) continue;
             else{
@@ -36,9 +34,8 @@ class Solution {
                 second = l.get(i);
             }
         }
-        System.out.print("\n"+first.val+" "+second.val);
-        temp.val = first.val;
+        int temp = first.val;
         first.val = second.val;
-        second.val = temp.val;
+        second.val = temp;
     }
 }
